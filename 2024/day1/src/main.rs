6b4ead2de24c0c23151b3c3027c187727922f1ce
Ayman <vymvn6@gmail.com>
2024-12-01 22:31:36 +0400

    d1p1 work

--- 2024/day1/src/main.rs
+++ 2024/day1/src/main.rs
@@ -1,3 +1,36 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut left : Vec<i32> = Vec::new();
+    let mut right : Vec<i32> = Vec::new();
+
+    let puzzle_input = fs::read_to_string("input.txt")
+        .expect("Couldn't read file");
+
+    let lines = puzzle_input.lines();
+
+    for line in lines {
+
+        let numbers: Vec<&str> = line.split_whitespace().collect();
+        left.push(numbers[0].parse::<i32>().unwrap());
+        right.push(numbers[1].parse::<i32>().unwrap());
+
+    }
+
+    left.iter()
+        .zip(right.iter())
+        .for_each(|(l, r)| println!("{} {}", l, r));
+
+    left.sort();
+    right.sort();
+
+    println!("===============================");
+
+    let total_diff: i32 = left.iter()
+        .zip(right.iter())
+        .map(|(l, r)| (*l - *r).abs())
+        .sum();
+
+    println!("Total diff: {}", total_diff);
 }
