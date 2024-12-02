22e39474bcac4d408367f9ed101d076a2f08354b
Ayman <vymvn6@gmail.com>
2024-12-02 19:47:30 +0400

    day 1 done

--- 2024/day1/src/main.rs
+++ 2024/day1/src/main.rs
@@ -1,12 +1,41 @@
 use std::fs;
 
+fn parse_input(filename: String) -> String {
+    let file_contents = fs::read_to_string(filename)
+        .expect("Couldn't read file");
+
+    return file_contents;
+}
+
+// Part 1
+fn find_total_diff(mut left: Vec<i32>, mut right: Vec<i32>) -> i32 {
+
+    left.sort();
+    right.sort();
+
+    let total_diff: i32 = left.iter()
+        .zip(right.iter())
+        .map(|(l, r)| (l - r).abs())
+        .sum();
+
+    return total_diff;
+}
+
+// Part 2
+fn find_similarity_score(left: Vec<i32>, right: Vec<i32>) -> i32 {
+    let similarity_score: i32 = left.iter()
+        .map(|l| right.iter().filter(|r| l == *r).count() as i32 * l)
+        .sum();
+
+    return similarity_score;
+}
+
 fn main() {
 
-    let mut left : Vec<i32> = Vec::new();
-    let mut right : Vec<i32> = Vec::new();
+    let puzzle_input = parse_input("input.txt".to_string());
 
-    let puzzle_input = fs::read_to_string("test_input.txt")
-        .expect("Couldn't read file");
+    let mut left : Vec<i32> = Vec::new();
+    let mut right: Vec<i32> = Vec::new();
 
     let lines = puzzle_input.lines();
 
@@ -18,25 +47,9 @@ fn main() {
 
     }
 
-    left.iter()
-        .zip(right.iter())
-        .for_each(|(l, r)| println!("{} || {}", l, r));
-
-    left.sort();
-    right.sort();
-
-    println!("==========================");
-
-    let total_diff: i32 = left.iter()
-        .zip(right.iter())
-        .map(|(l, r)| (*l - *r).abs())
-        .sum();
-
+    let total_diff = find_total_diff(left.clone(), right.clone());
     println!("Total diff: {}", total_diff);
 
-    let test: Vec<_> = left.iter()
-      .filter(|&l| !right.contains(l))
-      .collect();
-
-    println!("Test: {:?}", test);
+    let similarity_score = find_similarity_score(left.clone(), right.clone());
+    println!("Similarity score: {}", similarity_score);
 }
