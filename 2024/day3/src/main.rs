d084d8e1963fc68f668d2a3c33af5c8f68e05929
Ayman <vymvn6@gmail.com>
2024-12-05 01:19:00 +0400

    day3 done

--- 2024/day3/src/main.rs
+++ 2024/day3/src/main.rs
@@ -4,6 +4,8 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
+static mut MUL_ENABLED: bool = true;
+
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where
     P: AsRef<Path>,
@@ -16,6 +18,43 @@ fn mul(x: i32, y: i32) -> i32 {
     x * y
 }
 
+fn parse_instruction(cap: &regex::Captures) -> Option<(i32, i32)> {
+    if let (Ok(x), Ok(y)) = (cap[1].parse::<i32>(), cap[2].parse::<i32>()) {
+        Some((x, y))
+    } else {
+        None
+    }
+}
+
+fn find_instructions_plus_plus(line: String) -> Vec<(i32, i32)> {
+    let mut results: Vec<(i32, i32)> = Vec::new();
+
+    // unholy regex++ to match "mul(x,y)" and "do()" and "don't()"
+    let re = Regex::new(r"mul\((\d+),\s*(\d+)\)|do\(\)|don't\(\)").unwrap();
+
+    for cap in re.captures_iter(&line) {
+        unsafe {
+            match &cap[0] {
+                "do()" => {
+                    MUL_ENABLED = true;
+                }
+                "don't()" => {
+                    MUL_ENABLED = false;
+                }
+                _ => {
+                    if MUL_ENABLED {
+                        if let Some((x, y)) = parse_instruction(&cap) {
+                            results.push((x, y));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    results
+}
+
 fn find_instructions(line: String) -> Vec<(i32, i32)> {
     let mut results: Vec<(i32, i32)> = Vec::new();
 
@@ -23,7 +62,7 @@ fn find_instructions(line: String) -> Vec<(i32, i32)> {
     let re = Regex::new(r"mul\((-?\d+),\s*(-?\d+)\)").unwrap();
 
     for cap in re.captures_iter(&line) {
-        if let (Ok(x), Ok(y)) = (cap[1].parse::<i32>(), cap[2].parse::<i32>()) {
+        if let Some((x, y)) = parse_instruction(&cap) {
             results.push((x, y));
         }
     }
@@ -47,8 +86,8 @@ fn main() {
 
     if let Ok(lines) = read_lines(filename) {
         for line in lines.flatten() {
-            let instructions = find_instructions(line);
-            // multply all tuples and find sum
+            let instructions = find_instructions_plus_plus(line); // change to find_instructions()
+                                                                  // for part 1
             let sum: i32 = instructions.iter().map(|(x, y)| mul(*x, *y)).sum();
             sums.push(sum);
             println!("sum: {}", sum);
